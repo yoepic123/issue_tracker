3ff60fb86e02694fd98165036f1fe2a994710fa5
Tony Tran <tonytranmb@yahoo.com>
2024-10-14 17:22:11 -0700

    Show Issue Details

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -35,7 +35,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}> {issue.title} </Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
