764d0fb4396c5fe8fcfaa7f036c5d52a3d65c629
Tony Tran <tonytranmb@yahoo.com>
2024-10-17 14:14:06 -0700

    Build the filter component

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -75,7 +75,7 @@ const NavLinks = () => {
 
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Issues", href: "/issues/list" },
   ];
 
   return (
