fd00b3f93c8f1e8aee4b059a9dc1e5c8c4f22fc0
Tony Tran <tonytranmb@yahoo.com>
2024-10-17 15:55:36 -0700

    Paginate issues

--- issue-tracker/app/components/Pagination.tsx
+++ issue-tracker/app/components/Pagination.tsx
@@ -29,7 +29,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
   };
 
   return (
-    <Flex align="center" gap="2">
+    <Flex align="center" gap="2" className="py-3">
       <Text size="2">
         Page {currentPage} of {pageCount}{" "}
       </Text>
