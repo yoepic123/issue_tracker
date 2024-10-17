14a6d2ad3f3c896a2862e71af6603fc7202c0f3f
Tony Tran <tonytranmb@yahoo.com>
2024-10-17 13:39:55 -0700

    Enhance the API to assign issues

--- issue-tracker/app/api/issues/[id]/route.ts
+++ issue-tracker/app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,9 +12,18 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { assignedToUserID, title, description } = body;
+  if (assignedToUserID) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserID },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 404 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -26,8 +35,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserID,
     },
   });
 
